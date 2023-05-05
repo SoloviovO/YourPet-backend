9886955f10ab2bf091796346c0fdfaa91c7099a8
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-05 15:00:35 +0300

    final

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -1,4 +1,5 @@
 const { getNoticesList } = require("./noticesList");
+const { getOneNotice } = require("./notices-findOne");
 const { getCategory, addNotice } = require("./notices-category");
 
-module.exports = { getNoticesList, getCategory, addNotice };
+module.exports = { getNoticesList, getCategory, addNotice, getOneNotice };
