76aa6fd9bf253da9b7b4b48e743df8cb02df056c
VasylTehlivets <vasyltehlivets@gmail.com>
2023-05-08 01:23:13 +0300

    init notices19

--- routes/notice/index.js
+++ routes/notice/index.js
@@ -4,17 +4,27 @@ const noticesController = require("../../controllers/notices");
 
 const { schemas } = require("../../schemas/notice.schema");
 // const { schemas } = require("../../schemas/notice.schema");
-const { userAuthMiddleware, upload } = require("../../middlewares");
+const {
+  userAuthMiddleware,
+  upload,
+  uploadCloud,
+} = require("../../middlewares");
 const { controllerWrapper } = require("../../services");
 
 const router = express.Router();
 
 router.get("/", noticesController.getCategory);
 router.get("/find", controllerWrapper(noticesController.getNoticesList));
+router.get(
+  "/user",
+  userAuthMiddleware,
+  controllerWrapper(noticesController.getNoticesByOwnerId)
+);
 router.get("/:id", controllerWrapper(noticesController.getOneNotice));
 router.post(
   "/",
-  upload.single("image"),
+  // upload.single("image"),
+  uploadCloud.single("avatar"),
   userAuthMiddleware,
   controllerWrapper(noticesController.addNotice)
 );
