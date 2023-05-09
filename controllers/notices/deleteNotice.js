98bd392096b226481943508226dd16dc7066dc8e
SoloviovO <soloviov.o.m@gmail.com>
2023-05-09 22:13:23 +0300

    change for array

--- controllers/notices/deleteNotice.js
+++ controllers/notices/deleteNotice.js
@@ -5,9 +5,11 @@ const { createHttpException } = require("../../services");
 const deleteNotice = async (req, res) => {
   const { id } = req.params;
   const result = await NoticesModel.findByIdAndDelete(id);
+
   if (!result) {
     throw createHttpException(404, "Not found");
   }
+
   res.json({
     message: "Delete success",
   });
--- controllers/users/get-favorite.js
+++ controllers/users/get-favorite.js
@@ -6,10 +6,38 @@ const getFavoriteNotices = async (req, res, next) => {
   // const user = await UserModel.findById(_id).populate("favorite");
 
   // res.json({ user });
+  // const user = req.user;
+  // const { page, limit } = req.query;
+
+  // const result = await UserModel.findById(user._id);
+
+  // let favoriteNotices = result.favorite;
+
+  // if (page && limit) {
+  //   const startIndex = (page - 1) * limit;
+  //   const endIndex = page * limit;
+  //   favoriteNotices = favoriteNotices.slice(startIndex, endIndex);
+  // }
+
+  // const match = {
+  //   id: user._id,
+  //   email: user.email,
+  //   name: user.name,
+  //   birthday: user.birthday,
+  //   phone: user.phone,
+  //   city: user.city,
+  //   image: user.image,
+  //   favorite: user.favorite,
+  //   pets: user.pets,
+  //   notices: user.notices,
+  //   favorite: favoriteNotices,
+  // };
+
+  // res.json({ user: match });
   const user = req.user;
   const { page, limit } = req.query;
 
-  const result = await UserModel.findById(user._id);
+  const result = await UserModel.findById(user._id).populate("favorite");
 
   let favoriteNotices = result.favorite;
 
