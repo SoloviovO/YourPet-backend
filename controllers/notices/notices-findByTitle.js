5830c32c82c39095a6d61a641ec7d1eaf68c0655
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-05-10 11:34:17 +0300

    add search by category

--- controllers/notices/notices-findByTitle.js
+++ controllers/notices/notices-findByTitle.js
@@ -2,12 +2,12 @@ const { NoticesModel } = require("../../database/models");
 const { createHttpException } = require("../../services");
 
 const getNoticesByTitle = async (req, res) => {
-  const { title } = req.query;
+  const { title, category } = req.query;
   if (!title) {
     throw createHttpException(404, "Not found");
   }
   const regex = new RegExp(title, "i");
-  const result = await NoticesModel.find({ title: regex });
+  const result = await NoticesModel.find({ title: regex, category });
 
   if (result.length === 0) {
     throw createHttpException(404, "Not found");
