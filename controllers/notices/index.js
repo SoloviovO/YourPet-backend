b2f934402b3aa2d97f41c6a344f24acb638e142a
SoloviovO <soloviov.o.m@gmail.com>
2023-05-15 12:16:45 +0300

    add found title

--- controllers/notices/index.js
+++ controllers/notices/index.js
@@ -4,6 +4,8 @@ const { getCategory } = require("./getCategory");
 const { addNotice } = require("./addNotice");
 const { getNoticesByOwnerId } = require("./getNoticesByOwnerId");
 const { deleteNotice } = require("./deleteNotice");
+const { getOwnNoticesByTitle } = require("./own-notices-findByTitle");
+const { getFavoriteNoticesByTitle } = require("./favorit-notices-findByTitle");
 
 module.exports = {
   getNoticesByTitle,
@@ -12,4 +14,6 @@ module.exports = {
   getOneNotice,
   getNoticesByOwnerId,
   deleteNotice,
+  getOwnNoticesByTitle,
+  getFavoriteNoticesByTitle,
 };
